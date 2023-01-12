971f99d332356cbb42f7a3ffa99cca21e6ce6f3c
Jonny <alejandropanjon@gmail.co>
2023-01-12 13:01:21 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Firebase-citas-medicas',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
